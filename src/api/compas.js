bf86bf6fe37d08538163c24acabf3c1fed421ba6
tomvanmele <tom.v.mele@gmail.com>
2024-01-31 22:55:14 +0100

    api functions

--- src/api/compas.js
+++ src/api/compas.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
-const HOST = "http://127.0.0.1:8000/";
+const HOST = "http://localhost:8000";
 
 export default {
-  async fetchKeywords() {
-    let response = await axios.get(HOST + "api/brg/meta/keyword/");
+  async ping() {
+    let response = await axios.get(HOST + "/ping");
+    return response.data;
+  },
+
+  async boxToSubdividedMesh(boxdata) {
+    let response = await axios.post(HOST + "/box_to_subdivided_mesh", {
+      xsize: boxdata.xsize,
+      ysize: boxdata.ysize,
+      zsize: boxdata.zsize,
+    });
     return response.data;
   },
 };
